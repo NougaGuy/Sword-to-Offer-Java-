bdf1e3a238b256a9daa3d7ad13356fab7fbb301f
NougaGuy <852681432@qq.com>
2017-08-03 17:17:11 +0800

    First

--- src/Example/Test24ReverseList.java
+++ src/Example/Test24ReverseList.java
@@ -4,7 +4,7 @@ package Example;
  *Date:2017/8/2
  *Delcaration : All Rights Reserved ! 
  */
-public class Test24 {
+public class Test24ReverseList {
 
 	/**
 	 * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头结点。
@@ -15,11 +15,11 @@ public class Test24 {
 		ListNode next;
 	}
 	
-	public static ListNode ReverseList2 (ListNode head){
+	public static ListNode ReverseList2 (ListNode head) {
 		if (head == null){
 			return null ;
 		}
-		
+		//反转后的链表
 		ListNode reverseHead = null ;
 		//指针1：当前节点
 		ListNode currentNode = head ;
@@ -33,9 +33,27 @@ public class Test24 {
 			currentNode.next = prevNode;
 			prevNode = currentNode;
 			currentNode = nextNode;
+			System.out.println(currentNode.value);
 		}
 		return reverseHead;
 	}
-	
+	public static void main(String[] args) {
+		ListNode node1 = new ListNode();
+		node1.value = 1;
+		ListNode node2 = new ListNode();
+		node2.value = 2;
+		ListNode node3 = new ListNode();
+		node3.value = 3;
+		ListNode node4 = new ListNode();
+		node4.value = 4;
+		ListNode node5 = new ListNode();
+		node5.value = 5;
+		
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		ReverseList2(node1);
+	}
 }
  
