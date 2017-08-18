b3b1eae2f42e0928d0111fbd2009384a0c96e7a1
NougaGuy <852681432@qq.com>
2017-08-18 09:49:59 +0800

    剑指Offer部分实现

---
+++ src/Example/Test28isSymmetrical.java
@@ -0,0 +1,23 @@
+package Example;
+/**
+ * @author Nouga
+ *Date : 2017/8/18
+ *Declaration : All rights reserved ! 
+ */
+public class Test28isSymmetrical {
+
+	/**
+	 * 二叉树定义如下
+	 */
+	public static class BinaryTree {
+		int value;
+		BinaryTree left;
+		BinaryTree right;
+		@Override
+		public String toString() {
+			return value + " ";
+		}
+	}
+	
+	
+}
