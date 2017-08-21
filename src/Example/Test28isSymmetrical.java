5715973ee251397fceada4af58387320dd5ed3ca
NougaGuy <852681432@qq.com>
2017-08-21 19:52:13 +0800

    剑指Offer部分实现

--- src/Example/Test28isSymmetrical.java
+++ src/Example/Test28isSymmetrical.java
@@ -1,8 +1,7 @@
 package Example;
+
 /**
- * @author Nouga
- *Date : 2017/8/18
- *Declaration : All rights reserved ! 
+ * @author Nouga Date : 2017/8/18 Declaration : All rights reserved !
  */
 public class Test28isSymmetrical {
 
@@ -13,11 +12,39 @@ public class Test28isSymmetrical {
 		int value;
 		BinaryTree left;
 		BinaryTree right;
+
 		@Override
 		public String toString() {
 			return value + " ";
 		}
 	}
-	
-	
+
+	public static boolean compare(BinaryTree tree) {
+		// 我的思路：前序遍历（根，左，右） 对比 （根，右，左）
+		// 网上思路：鲁棒性检测，判断节点值是否相同。
+		if (tree == null)
+			return true;
+
+		return compareNode(tree.left, tree.right);
+	}
+
+	private static boolean compareNode(BinaryTree left, BinaryTree right) {
+		if (left == null && right == null)
+			return true;
+		if (left == null || right == null)
+			return false;
+		return left.value == right.value && 
+				compareNode(left.left, right.right) && 
+				compareNode(left.right, right.left);
+	}
+
+	// 前序遍历
+	public static void preOrder(BinaryTree tree) {
+		if (tree != null) {
+			tree.toString();
+			preOrder(tree.left);
+			preOrder(tree.right);
+		}
+	}
+
 }
